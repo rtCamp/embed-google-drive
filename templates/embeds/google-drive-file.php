279a667f5b84246583f30433b169ee1f09852f78
Abhishek Kumar <abhishekfdd@gmail.com>
2020-11-04 11:36:18 +0530

    updated preview template

--- templates/embeds/google-drive-file.php
+++ templates/embeds/google-drive-file.php
@@ -9,10 +9,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 	// prevent direct access to this file.
 	exit;
 }
-?>
-<div style="border: 1px solid #000; text-align: center;">
-	<a href="<?php echo esc_url( $drive_file_url ); ?>" title="<?php esc_attr_e( 'Open the Shared Document', 'rt-google-embeds' ); ?>" target="_blank" rel="noopener noreferrer" style="color: #cd2653;">
-		<?php esc_html_e( 'Open Shared Document.', 'rt-google-embeds' ); ?>
-	</a>
-	<img src="<?php echo esc_url( $thumbnail_url ); ?>" alt="<?php esc_attr_e( 'Shared Document Preview', 'rt-google-embeds' ); ?>" style="border: 1px solid #eee; margin: 15px auto; display: block;" />
-</div>
+if ( file_exists( $cached_file ) ) : ?>
+	<div style="border: 1px solid #000; text-align: center;">
+		<a href="<?php echo esc_url( $drive_file_url ); ?>" title="<?php esc_attr_e( 'Open the Shared Document', 'rt-google-embeds' ); ?>" target="_blank" rel="noopener noreferrer" style="color: #cd2653;">
+			<?php esc_html_e( 'Open Shared Document.', 'rt-google-embeds' ); ?>
+		</a>
+		<img src="<?php echo esc_url( $cached_url ); ?>" alt="<?php esc_attr_e( 'Shared Document Preview from Cache', 'rt-google-embeds' ); ?>" style="border: 1px solid #eee; margin: 15px auto; display: block;" />
+	</div>
+<?php else : ?>
+	<div style="border: 1px solid #000; text-align: center;">
+		<a href="<?php echo esc_url( $drive_file_url ); ?>" title="<?php esc_attr_e( 'Open the Shared Document', 'rt-google-embeds' ); ?>" target="_blank" rel="noopener noreferrer" style="color: #cd2653;">
+			<?php esc_html_e( 'Open Shared Document.', 'rt-google-embeds' ); ?>
+		</a>
+		<img src="<?php echo esc_url( $thumbnail_url ); ?>" alt="<?php esc_attr_e( 'Shared Document Preview', 'rt-google-embeds' ); ?>" style="border: 1px solid #eee; margin: 15px auto; display: block;" />
+	</div>
+<?php endif; ?>
