2e80ca050c78c317a16374be844b581539e42bb9
Paul David Clark <pdclark@pdclark.com>
2022-01-20 08:25:38 -0800

    Remove unnecessary periods.

--- templates/embeds/google-drive-file.php
+++ templates/embeds/google-drive-file.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <div style="border: 1px solid #000; text-align: center;">
 	<a href="<?php echo esc_url( $drive_file_url ); ?>" title="<?php esc_attr_e( 'Open the Shared Document', 'rt-google-embeds' ); ?>" target="_blank" rel="noopener noreferrer" style="color: #cd2653;">
-		<?php esc_html_e( 'Open Shared Document.', 'rt-google-embeds' ); ?>
+		<?php esc_html_e( 'Open Shared Document', 'rt-google-embeds' ); ?>
 	</a>
 	<img src="<?php echo esc_url( $thumbnail_url ); ?>" alt="<?php esc_attr_e( 'Shared Document Preview', 'rt-google-embeds' ); ?>" style="border: 1px solid #eee; margin: 15px auto; display: block;" />
 </div>
