09fa2ac68598e805ce6ec4b13b5bc62ce56129e3
Shalin Shah <sahilshh723@gmail.com>
2023-12-14 18:20:12 +0530

    Update: Text domain, function parameters and return types updated

--- embed-google-drive.php
+++ embed-google-drive.php
@@ -2,10 +2,10 @@
 /**
  * Plugin Name: Embed Google Drive
  * Description: Embed a link and preview of Google Drive Documents by pasting a shared document link into the editor.
- * Plugin URI: https://github.com/rtCamp/rt-google-embeds
+ * Plugin URI: https://github.com/rtCamp/embed-google-drive
  * Version: 1.0
  * Author: rtCamp
- * Text Domain: rt-google-embeds
+ * Text Domain: embed-google-drive
  * Author URI: https://rtcamp.com/
  * Domain Path: /languages/
  *
@@ -18,6 +18,14 @@ if ( ! defined( 'RT_GOOGLE_EMBEDS_PLUGIN_FILE' ) ) {
 	define( 'RT_GOOGLE_EMBEDS_PLUGIN_FILE', __FILE__ );
 }
 
-if ( ! class_exists( 'rtCamp_Google_Embeds' ) ) {
+if ( ! defined( 'RT_GOOGLE_EMBEDS_VERSION' ) ) {
+	define( 'RT_GOOGLE_EMBEDS_VERSION', '1.2' );
+}
+
+if ( ! defined( 'RT_GOOGLE_EMBEDS_PLUGIN_DIR' ) ) {
+	define( 'RT_GOOGLE_EMBEDS_PLUGIN_DIR', plugin_dir_path( RT_GOOGLE_EMBEDS_PLUGIN_FILE ) );
+}
+
+if ( ! class_exists( 'RtCamp_Google_Embeds' ) ) {
 	require_once dirname( __FILE__ ) . '/includes/classes/class-rtcamp-google-embeds.php';
 }
--- templates/embeds/google-drive-file.php
+++ templates/embeds/google-drive-file.php
@@ -9,10 +9,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 	// prevent direct access to this file.
 	exit;
 }
+
+if ( ! isset( $drive_file_url ) || ! isset( $thumbnail_url ) ) {
+	return;
+}
+
 ?>
 <div style="border: 1px solid #000; text-align: center;">
-	<a href="<?php echo esc_url( $drive_file_url ); ?>" title="<?php esc_attr_e( 'Open the Shared Document', 'rt-google-embeds' ); ?>" target="_blank" rel="noopener noreferrer" style="color: #cd2653;">
-		<?php esc_html_e( 'Open Shared Document', 'rt-google-embeds' ); ?>
+	<a href="<?php echo esc_url( $drive_file_url ); ?>" title="<?php esc_attr_e( 'Open the Shared Document', 'embed-google-drive' ); ?>" target="_blank" rel="noopener noreferrer" style="color: #cd2653;">
+		<?php esc_html_e( 'Open Shared Document', 'embed-google-drive' ); ?>
 	</a>
-	<img src="<?php echo esc_url( $thumbnail_url ); ?>" alt="<?php esc_attr_e( 'Shared Document Preview', 'rt-google-embeds' ); ?>" style="border: 1px solid #eee; margin: 15px auto; display: block;" />
+	<img src="<?php echo esc_url( $thumbnail_url ); ?>" alt="<?php esc_attr_e( 'Shared Document Preview', 'embed-google-drive' ); ?>" style="border: 1px solid #eee; margin: 15px auto; display: block;" />
 </div>
