127c1ac04b7bab94c8d72dc69e2b33df86995b5c
Anuj-Rathore24 <anujsingh2409@gmail.com>
2025-05-27 12:10:15 +0530

    feat: Restore previously removed file: [rt-google-embeds.php]

---
+++ rt-google-embeds.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: Embed Google Drive
+ * Description: Embed a link and preview of Google Drive Documents by pasting a shared document link into the editor.
+ * Plugin URI: https://github.com/rtCamp/embed-google-drive
+ * Version: 1.2.1
+ * Author: rtCamp
+ * Text Domain: rt-google-embeds
+ * Author URI: https://rtcamp.com/
+ * Domain Path: /languages/
+ *
+ * @package rt-google-embeds
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( ! defined( 'RT_GOOGLE_EMBEDS_PLUGIN_FILE' ) ) {
+	define( 'RT_GOOGLE_EMBEDS_PLUGIN_FILE', __FILE__ );
+}
+
+if ( ! defined( 'RT_GOOGLE_EMBEDS_VERSION' ) ) {
+	define( 'RT_GOOGLE_EMBEDS_VERSION', '1.2.1' );
+}
+
+if ( ! defined( 'RT_GOOGLE_EMBEDS_PLUGIN_DIR' ) ) {
+	define( 'RT_GOOGLE_EMBEDS_PLUGIN_DIR', plugin_dir_path( RT_GOOGLE_EMBEDS_PLUGIN_FILE ) );
+}
+
+if ( ! class_exists( 'RT_Google_Embeds\rtCamp_Google_Embeds' ) ) {
+	require_once RT_GOOGLE_EMBEDS_PLUGIN_DIR . '/includes/classes/class-rtcamp-google-embeds.php';
+}
